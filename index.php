20744fe7a6e4e2367bdf120e6b51ef50e23ca82d
PedradasDaTuga <pedromiguelcorreia2002@gmail.com>
2022-04-08 16:00:07 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <title>LTW Project</title>
+        <meta charset="utf-8">
+        <!-- <link rel="stylesheet" href="style.css">  -->
+    </head>
+    <body>
+        <header>
+            <h1><a=href="index.php">NOME PAGINA</a></h1>
+            <form action="/" method="post" class="login">
+                <input type="text" name="username" placeholder="username">
+                <input type="password" name="password" placeholder="password">
+                <a href="register.php">Register</a>
+                <button type="submit">Login</button>
+            </form>
+        </header>
+    
+        <main>
+
+
+
+
+
+
+    </body>
+
+
+
+
+
+
+
+
+</html>
